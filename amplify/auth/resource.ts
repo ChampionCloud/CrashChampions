87820d35d47b152e123f52e65a676cd32820903f
ChampionCloud <152559446+ChampionCloud@users.noreply.github.com>
2023-12-01 11:25:18 -0700

    Update resource.ts

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,41 +1,71 @@
-import { defineAuth } from '@aws-amplify/backend';
+<!DOCTYPE html>
+<html>
+<body>
 
-/**
- * Define and configure your auth resource
- * When used alongside data, it is automatically configured as an auth provider for data
- * @see https://docs.amplify.aws/gen2/build-a-backend/auth
- */
-export const auth = defineAuth({
-  loginWith: {
-    email: true,
-    // add social providers
-    externalProviders: {
-      /**
-       * first, create your secrets using `amplify sandbox secret`
-       * then, import `secret` from `@aws-amplify/backend`
-       * @see https://docs.amplify.aws/gen2/deploy-and-host/sandbox-environments/features/#setting-secrets
-       */
-      // loginWithAmazon: {
-      //   clientId: secret('LOGINWITHAMAZON_CLIENT_ID'),
-      //   clientSecret: secret('LOGINWITHAMAZON_CLIENT_SECRET'),
-      // }
-    },
-  },
-  /**
-   * enable multifactor authentication
-   * @see https://docs.amplify.aws/gen2/build-a-backend/auth/manage-mfa
-   */
-  // multifactor: {
-  //   mode: 'OPTIONAL',
-  //   sms: {
-  //     smsMessage: (code) => `Your verification code is ${code}`,
-  //   },
-  // },
-  userAttributes: {
-    /** request additional attributes for your app's users */
-    // profilePicture: {
-    //   mutable: true,
-    //   required: false,
-    // },
-  },
-});
+<div id="qr" style="display:inline-block; align: center;vertical-align: middle;" >
+  <img src="https://app.box.com/shared/static/f23q4tph1yxeatxlf4fwzqflswp1wg9e.png" width="100" height="100">
+</div>
+<div style="display:inline-block;vertical-align: middle;">
+  <font color="White"><font size="22">Crash Champions - Estimating Essentials </font>
+</div>
+
+
+  <title>Crash Champions - Estimating Essentials</title>
+  <style>
+    body {background-color: black;}
+    h1 {color: white;}
+    p {color: white;}
+  </style>
+</head>  
+<body>
+
+<link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
+<style>
+body {
+    font-family: 'Montserrat';font-size: 22px;
+}
+
+
+</style>
+</head>
+<body>
+
+
+<p> - Click and drag the image, to rotate and move around the vehicle.  Select the other images to review additional damage on the vehicle.</p>
+
+</style>
+</head>
+<body>
+
+
+
+</body>
+</html>
+
+
+<div class="Sirv" data-options="zoom.map.enable:true; spin.speed:10000; quality:100; hdQuality:93; lazyImage.quality:100; lazyImage.hdQuality:100">
+  <div data-src="https://crashchampions.sirv.com/Spins/Hyundai/Hyundai.spin"></div>
+  <div data-src="https://crashchampions.sirv.com/Spins/Hyundai-Fender/Hyundai-Fender.spin"></div>
+  <div data-src="https://crashchampions.sirv.com/Spins/Hyundai-Door/Hyundai-Door.spin"></div>
+  <div data-src="https://crashchampions.sirv.com/Spins/Hyundai-Quarter/Hyundai-Quarter.spin"></div>
+  <div data-src="https://crashchampions.sirv.com/Spins/Hyundai-Decklid/Hyundai-Decklid.spin"></div>
+  <div data-src="https://crashchampions.sirv.com/Spins/Hyundai-RT-Qtr/Hyundai-RT-Qtr.spin"></div>
+  <div data-src="https://crashchampions.sirv.com/Spins/IMG_5555.jpeg" data-type="zoom"></div>
+  <div data-src="https://crashchampions.sirv.com/Spins/IMG_5556.jpeg" data-type="zoom"></div>
+  <div data-src="https://crashchampions.sirv.com/Spins/IMG_5557.jpeg" data-type="zoom"></div>
+  <div data-src="https://crashchampions.sirv.com/Spins/IMG_5560.jpeg" data-type="zoom"></div>
+  <div data-src="https://crashchampions.sirv.com/Spins/IMG_5561.jpeg" data-type="zoom"></div>
+</div>
+<script src="https://scripts.sirv.com/sirvjs/v3/sirv.js"></script>
+
+<style>
+ .smv-button-fullscreen-open .smv-icon, .smv-button-fullscreen-close .smv-icon {
+   background-color: rgba(0, 0, 0, 1);
+ }
+ .smv-fullscreen-box {
+   background-color: rgba(0, 0, 0, 0.97);
+ }
+</style>
+
+</body>
+</html> 
